b229c9f4864026daf28c9970f7f644ae4d2b61a1
Venki <18534venkanna@gmail.com>
2025-02-23 19:31:43 +0530

    Camera Setup

--- Source/CombatSystem/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/CombatSystem/Private/Characters/WarriorHeroCharacter.cpp
@@ -2,4 +2,38 @@
 
 
 #include "Characters/WarriorHeroCharacter.h"
+#include "DebugHelper.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
+AWarriorHeroCharacter::AWarriorHeroCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 200.0f;
+	CameraBoom->SocketOffset = FVector(0.f,55.0f,65.0f);
+	CameraBoom->bUsePawnControlRotation = true;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(CameraBoom,USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f,500.0f,0.0f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.0f;
+}
+
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
--- Source/CombatSystem/Public/Characters/WarriorHeroCharacter.h
+++ Source/CombatSystem/Public/Characters/WarriorHeroCharacter.h
@@ -6,12 +6,24 @@
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class COMBATSYSTEM_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AWarriorHeroCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* Camera;
+	
+
+	
 	
 };
