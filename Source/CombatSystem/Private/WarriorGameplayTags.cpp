aa30496f1ebc2892d78d8066660d1d45893c85a3
Venki <18534venkanna@gmail.com>
2025-02-23 20:18:30 +0530

    Native Gameplay Tags

---
+++ Source/CombatSystem/Private/WarriorGameplayTags.cpp
@@ -0,0 +1,11 @@
+// Venkanna Babu All Rights Reserved
+
+
+#include "WarriorGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	/*Input Tags*/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move,"InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look,"InputTag.Look")
+}
---
+++ Source/CombatSystem/Public/WarriorGameplayTags.h
@@ -0,0 +1,12 @@
+// Venkanna Babu All Rights Reserved
+
+#pragma once
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	/*Input Tags*/
+	COMBATSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	COMBATSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+	
+}
